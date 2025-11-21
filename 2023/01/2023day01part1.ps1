928599a2d8a063096c9c131d51361d433748bd47
Jakob Trier <jakob.trier@gmail.com>
2025-11-21 07:15:15 +0100

    Update and rename 2023/2023day01part1.ps1 to 2023/01/2023day01part1.ps1

--- 2023/01/2023day01part1.ps1
+++ 2023/01/2023day01part1.ps1
@@ -1,7 +1,6 @@
 # By JATR, 2025.08.19
 # https://adventofcode.com/2023/day/1
 
-
 # Load the data
 $puzzledata = Get-Content C:\jatr\2023day1.dat
 
@@ -11,9 +10,8 @@ $answer = 0
 # Go through the data one line at the time
 $puzzledata | Foreach {
 	$linedata = $_
-	$nodatafound = 1
+	$nodatafound = 1   
 	for ($i=0;$i -lt $linedata.Length;$i++) {
-	   
 	   if ( $linedata[$i] -match "^\d+$") { # if it is a number 
 	     if ( $nodatafound -eq 1 ) { # first time we find a number in this linedata
 			 $nodatafound = 0 # we have now found a number
