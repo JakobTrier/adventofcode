56d2bd0b417d2e6cde616351505644630fb442e0
Jakob Trier <jakob.trier@gmail.com>
2025-11-21 06:37:26 +0100

    Update and rename 2024/2024day3part2.ps1 to 2024/03/2024day03part2.ps1

---
+++ 2024/03/2024day03part2.ps1
@@ -0,0 +1,18 @@
+$puzzledata = Get-Content 2024day1.dat
+
+$puzzleInput = $puzzleInput -replace "don't\(\)(.|\n)*?do\(\)",""
+
+$regex = [regex] 'mul\(\d{1,3},\d{1,3}\)'
+
+$allMatches = $regex.Matches($puzzleInput)
+
+$total = 0
+
+foreach ($match in $allMatches) {
+    $tempStr = $match.Value
+    $values = $tempStr.Replace("mul(","").Replace(")","")
+    $thisSum = [int]$values.Split(",")[0] * [int]$values.Split(",")[1]
+    $total += $thisSum
+}
+
+$total
