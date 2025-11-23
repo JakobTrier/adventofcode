2709799c861e1ae282ee0189404fb9f22eae0e98
Jakob Trier <jakob.trier@gmail.com>
2025-11-23 15:12:11 +0100

    Create 2019day03part2.ps1

---
+++ 2019/03/2019day03part2.ps1
@@ -0,0 +1,165 @@
+# By JATR, 2025.11.03-04
+# https://adventofcode.com/2019/day/3#part2
+
+# Load the data
+$puzzledata = Get-Content 2019day3.dat
+
+# We start by finding out how large a grid we need..
+
+$maxx = 0
+$maxy = 0
+
+$minx = 0
+$miny = 0
+
+$puzzledata | Foreach { 
+ $xpos = 0
+ $ypos = 0
+ $_.split(",") | Foreach {
+  $number = $_.Substring(1)
+  switch ($_[0])
+  {
+    "L"	{ 
+		   $xpos = $xpos - $number
+		   if ($xpos -lt $minx) { $minx = $xpos }
+		} 
+	"R" {
+		   $xpos = $xpos + $number
+		   if ($xpos -gt $maxx) { $maxx = $xpos }
+	    }
+	"D" {
+		   $ypos = $ypos - $number
+		   if ($ypos -lt $miny) { $miny = $ypos }
+	    }
+	"U" {
+		   $ypos = $ypos + $number
+		   if ($ypos -gt $maxy) { $maxy = $ypos }
+	    }
+  }
+ }
+}
+
+$sizex = $maxx - $minx
+$sizey = $maxy - $miny
+
+$mindist = $sizex * $sizey
+
+Write-Host "("$sizex","$sizey ")"
+# ( 12534,18459 )
+# Create the matrix...
+$Multi = New-Object -TypeName 'Int[,]' -ArgumentList ($sizex+1),($sizey+1)
+
+
+# x: -7606  -> 4928
+# y: -15667 -> 2792
+
+# where is origon.... 
+$ox = 0 - $minx 
+$oy = 0 - $miny 
+
+$Multi[$ox,$oy] = 1
+
+$steps = 0
+
+# Draw line 1
+
+# we start at origon
+ $xpos = $ox
+ $ypos = $oy
+ $puzzledata[0].split(",") | Foreach { 
+ Write-host "(" $xpos "," $ypos ")" $_
+ 
+ $number = $_.Substring(1)
+  switch ($_[0])
+  {
+    "L"	{ 
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $xpos--
+			 $steps++
+			 $Multi[$xpos,$ypos] = $steps
+		   }
+		} 
+	"R" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $xpos++
+			 $steps++
+			 $Multi[$xpos,$ypos] = $steps
+		   }
+	    }
+	"D" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $ypos--
+			 $steps++
+			 $Multi[$xpos,$ypos] = $steps
+		   }
+	    }
+	"U" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $ypos++
+			 $steps++
+			 $Multi[$xpos,$ypos] = $steps
+		   }
+	    }
+  }
+
+ }
+
+# Find where line 2 crosses line 1..
+
+# we start at origon - again
+
+ $xpos = $ox
+ $ypos = $oy
+ $steps = 0
+  
+ $puzzledata[1].split(",") | Foreach { 
+
+
+ $number = $_.Substring(1)
+  switch ($_[0])
+  {
+    "L"	{ 
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $xpos--
+			 $steps++
+			 if ( $Multi[$xpos,$ypos] -gt 0 ) {
+				Write-host "X: (" $xpos "," $ypos ") Dist: " $([Math]::Abs($xpos-$ox) + [Math]::Abs($ypos-$oy)) " !! " $($Multi[$xpos,$ypos] + $steps) " ( " $Multi[$xpos,$ypos] " + " $steps " )"
+                if ( ($Multi[$xpos,$ypos] + $steps) -lt $mindist) { $mindist = $Multi[$xpos,$ypos] + $steps } 		
+			 }
+		   }
+		} 
+	"R" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $xpos++
+			 $steps++
+			 if ( $Multi[$xpos,$ypos] -gt 0 ) {
+				Write-host "X: (" $xpos "," $ypos ") Dist: " $([Math]::Abs($xpos-$ox) + [Math]::Abs($ypos-$oy)) " !! " $($Multi[$xpos,$ypos] + $steps) " ( " $Multi[$xpos,$ypos] " + " $steps " )" 
+                if ( ($Multi[$xpos,$ypos] + $steps) -lt $mindist) { $mindist = $Multi[$xpos,$ypos] + $steps }				
+			 }
+		   }
+	    }
+	"D" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $ypos--
+			 $steps++
+			 if ( $Multi[$xpos,$ypos] -gt 0 ) {
+				Write-host "X: (" $xpos "," $ypos ") Dist: " $([Math]::Abs($xpos-$ox) + [Math]::Abs($ypos-$oy)) " !! " $($Multi[$xpos,$ypos] + $steps) " ( " $Multi[$xpos,$ypos] " + " $steps " )"     
+				if ( ($Multi[$xpos,$ypos] + $steps) -lt $mindist) { $mindist = $Multi[$xpos,$ypos] + $steps }
+			 }
+		   }
+	    }
+	"U" {
+		   for ($i = 0 ; $i -lt $number ; $i++) { 
+		     $ypos++
+			 $steps++
+			 if ( $Multi[$xpos,$ypos] -gt 0 ) {
+				Write-host "X: (" $xpos "," $ypos ") Dist: " $([Math]::Abs($xpos-$ox) + [Math]::Abs($ypos-$oy)) " !! " $($Multi[$xpos,$ypos] + $steps) " ( " $Multi[$xpos,$ypos] " + " $steps " )"
+				if ( ($Multi[$xpos,$ypos] + $steps) -lt $mindist) { $mindist = $Multi[$xpos,$ypos] + $steps }
+			 }
+		   }
+	    }
+  }
+
+ }
+ 
+ Write-host "Answer: " $mindist
