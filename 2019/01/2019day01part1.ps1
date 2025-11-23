5bbaa2d0bbcf8e163591a9dab0d19a1c422fe040
Jakob Trier <jakob.trier@gmail.com>
2025-11-23 15:07:43 +0100

    Create 2019day01part1.ps1

---
+++ 2019/01/2019day01part1.ps1
@@ -0,0 +1,19 @@
+# By JATR, 2025.08.20
+# https://adventofcode.com/2019/day/1
+
+# Load the data
+$puzzledata = Get-Content 2019day1.dat
+
+$alldata = $null
+$answer = 0
+
+$puzzledata | Foreach { # load all data into an array
+	$alldata = $alldata + @($_)
+}
+  for ($i=0; $i -lt $alldata.count; $i++) { # Go through the data
+    $fuel = [Math]::Floor($alldata[$i]/3) - 2
+	$answer = $answer + $fuel
+	write-host $i $alldata[$i] $fuel
+   }
+
+Write-Host $answer
